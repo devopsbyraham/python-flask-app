548655783452d2056c53f92acacf948f3b7456ad
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-07-03 21:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
-version = os.environ.get('APP_VERSION', '1.0.0')
+version = os.environ.get('APP_VERSION', '2.0.0')
 
 @app.route('/health')
 def health_check():
@@ -10,7 +10,7 @@ def health_check():
 
 @app.route('/')
 def home():
-    return jsonify({"message": "THIS IS APP V-1.0"}), 200
+    return jsonify({"message": "THIS IS APP V-2.0"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
